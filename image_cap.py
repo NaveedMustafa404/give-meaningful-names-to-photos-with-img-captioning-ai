4d5186dd868a8db5a28494c1abebc6d9b742320f
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2025-09-06 20:27:47 +0600

    Added caption generation with step-by-step word-by-word printing

--- image_cap.py
+++ image_cap.py
@@ -1,6 +1,7 @@
 import requests
+import threading
 from PIL import Image
-from transformers import AutoProcessor, BlipForConditionalGeneration
+from transformers import AutoProcessor, BlipForConditionalGeneration, TextStreamer, TextIteratorStreamer
 
 # Here we load the pretrained processor and model
 processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
@@ -15,11 +16,25 @@ image = Image.open(img_path).convert('RGB')
 text = "the image of"
 inputs = processor(images=image, text=text, return_tensors="pt")
 
+# Using TextStreamer to create a streamer that prints tokens as they are generated
+streamer = TextStreamer(processor.tokenizer, skip_special_tokens=True)
+
 # Caption generating 
-#BLIP model runs a transformer decoder that predicts the caption word by word
-outputs = model.generate(**inputs, max_length=50)
+# BLIP model runs a transformer decoder that predicts the caption word by word
+outputs = model.generate(**inputs, max_length=50, streamer=streamer)
+
+token_ids = outputs[0]
+
+print("Streaming caption:")
+words = []
+for i in range(len(token_ids)):
+    chunk = processor.decode(token_ids[:i+1], skip_special_tokens=True)
+    if chunk and (not words or chunk != " ".join(words)):  
+        words = chunk.split()
+        print(" ".join(words))
 
 # Here decoding the generated tokens to readable text 
 caption = processor.decode(outputs[0], skip_special_tokens=True)
 # Print the caption
-print(caption)
+print("\nFinal Caption:", caption)
+
